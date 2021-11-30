fbf5f190186061a3c8defd93da9d9efff35e4bb1
Rei Fukuoka <93930296+Refuikuoka@users.noreply.github.com>
2021-11-30 17:06:34 +0000

    Part 1 - I added another code for a third user input, and also another if command.
    Part 2 - I added colons, equation marks, and also an else command at the end.
    
    Part 3 - I wrote a code with < > == and % signs.
    
    Part 4 - I wrote this code by using < > == signs, and also and/or commands.
    
    Part 5 - Same as part 4

--- part3.py
+++ part3.py
@@ -20,3 +20,19 @@ not divisible by 3
 '''
 
 #write your code below
+number = int(input("Enter a number: "))
+
+if number > 0 :
+  print("positive")
+
+elif number == 0 :
+  print("Zero")
+
+elif number < 0 :
+  print("negative")
+
+
+if number % 3 == 2 or 1 :
+  print("not divisible by 3")
+else :
+  print("divisible by 3")
